43ff970f1214576b767438f6168889b8da0a3976
shubham-tam <tamrakarshubham23@gmail.com>
2023-04-21 16:54:54 +0545

    Title error removed

--- src/components/UI/Title.tsx
+++ src/components/UI/Title.tsx
@@ -7,16 +7,16 @@ const Title = (props: TitleProps) => {
   const { title, identifier } = props;
 
   if (identifier !== "region") {
-    <div className="text-center text-7xl p-5  text-slate-200">{title}</div>;
-  }
-
-  return (
-    identifier === "region" && (
+    return (
+      <div className="text-center text-7xl p-5  text-slate-200">{title}</div>
+    );
+  } else {
+    return (
       <div className="text-center text-7xl p-5 font-bold text-slate-600">
         {title}
       </div>
-    )
-  );
+    );
+  }
 };
 
 export default Title;
